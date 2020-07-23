1affa6c849458ba3ed0739166f6ec4e5db3f2ef6
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-23 02:14:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
 countdown = 10 
 while countdown <= 10 do
-  break if countdown == 1
+  break if countdown == 0
   puts countdown
   countdown = countdown - 1
   puts "Happy New Year!"
